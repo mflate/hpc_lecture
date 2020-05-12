72395c492d7420a7bc49e902f4e02d0b4a8db63c
mflate <magnufh@stud.ntnu.no>
2020-05-12 04:54:30 +0200

    Implemented parallelism in 12, 13 and 14

--- 02_openmp/13_merge_sort.cpp
+++ 02_openmp/13_merge_sort.cpp
@@ -7,6 +7,7 @@ void merge(std::vector<T>& vec, int begin, int mid, int end) {
   std::vector<T> tmp(end-begin+1);
   int left = begin;
   int right = mid+1;
+#pragma omp for
   for (int i=0; i<tmp.size(); i++) { 
     if (left > mid)
       tmp[i] = vec[right++];
@@ -17,6 +18,8 @@ void merge(std::vector<T>& vec, int begin, int mid, int end) {
     else
       tmp[i] = vec[right++]; 
   }
+
+#pragma omp for
   for (int i=0; i<tmp.size(); i++) 
     vec[begin++] = tmp[i];
 }
@@ -25,8 +28,11 @@ template<class T>
 void merge_sort(std::vector<T>& vec, int begin, int end) {
   if(begin < end) {
     int mid = (begin + end) / 2;
+#pragma omp task shared(vec)
     merge_sort(vec, begin, mid);
+#pragma omp task shared(vec)
     merge_sort(vec, mid+1, end);
+#pragma omp taskwait
     merge(vec, begin, mid, end);
   }
 }
@@ -40,7 +46,11 @@ int main() {
   }
   printf("\n");
 
-  merge_sort(vec, 0, n-1);
+#pragma omp parallell
+  {
+#pragma omp single
+    merge_sort(vec, 0, n-1);
+  }
 
   for (int i=0; i<n; i++) {
     printf("%d ",vec[i]);
--- 02_openmp/14_bucket_sort.cpp
+++ 02_openmp/14_bucket_sort.cpp
@@ -1,6 +1,7 @@
 #include <cstdio>
 #include <cstdlib>
 #include <vector>
+#include <omp.h>
 
 int main() {
   int n = 50;
@@ -13,14 +14,24 @@ int main() {
   printf("\n");
 
   std::vector<int> bucket(range); 
+#pragma omp parallel for
   for (int i=0; i<range; i++) {
     bucket[i] = 0;
   }
-  for (int i=0; i<n; i++) {
+#pragma omp parallel for shared(bucket)
+  for (int i=0; i<n; i++) 
+  {
+#pragma omp atomic update
     bucket[key[i]]++;
+    
   }
-  for (int i=0, j=0; i<range; i++) {
-    for (; bucket[i]>0; bucket[i]--) {
+#pragma omp parallel for
+  for (int i=0; i<range; i++) {
+    int j = 0;
+    for (int k = 0; k<i; k++) {
+      j += bucket[k];
+    }
+    for (int b = bucket[i]; b>0; b--) {
       key[j++] = i;
     }
   }

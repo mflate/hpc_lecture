61a4e2ac330f4774e34b93fad0412e6f72dfb411
mflate <magnufh@stud.ntnu.no>
2020-05-14 14:34:04 +0200

    Rewrote 11_distributed.cpp using MPI_Put

--- 03_mpi/11_distributed.cpp
+++ 03_mpi/11_distributed.cpp
@@ -21,14 +21,21 @@ int main(int argc, char** argv) {
     ibody[i].m = jbody[i].m = drand48();
     ibody[i].fx = jbody[i].fx = ibody[i].fy = jbody[i].fy = 0;
   }
+  MPI_Win win; // Window variable
   int recv_from = (rank + 1) % size;
   int send_to = (rank - 1 + size) % size;
   MPI_Datatype MPI_BODY;
   MPI_Type_contiguous(5, MPI_DOUBLE, &MPI_BODY);
   MPI_Type_commit(&MPI_BODY);
   for(int irank=0; irank<size; irank++) {
-    MPI_Send(jbody, N/size, MPI_BODY, send_to, 0, MPI_COMM_WORLD);
-    MPI_Recv(jbody, N/size, MPI_BODY, recv_from, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    
+    // Changed code for assignment starts here
+    MPI_Win_create(jbody, (N/size)*sizeof(MPI_BODY), sizeof(MPI_BODY), MPI_INFO_NULL, MPI_COMM_WORLD, &win);
+    MPI_Win_fence(0, win);
+    MPI_Put(jbody, N/size, MPI_BODY, send_to, 0, N, MPI_BODY, win);
+    MPI_Win_fence(0, win);
+    // Ends here
+
     for(int i=0; i<N/size; i++) {
       for(int j=0; j<N/size; j++) {
         double rx = ibody[i].x - jbody[j].x;
